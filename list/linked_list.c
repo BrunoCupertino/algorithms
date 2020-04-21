6292c938b068c78b25d5c32c00afe70cc875f910
Bruno Cupertino <bruno.cupertino@gmail.com>
2020-04-21 19:43:02 -0300

    add get last methods

--- list/linked_list.c
+++ list/linked_list.c
@@ -7,6 +7,10 @@ struct Node
     struct Node* next;
 };
 
+void printItem(struct Node* node) {
+    printf(" data:%d ", node->data);
+}
+
 void printList(struct Node* node) {
     //is it recursive function better than loops?
     // if(node != NULL) {
@@ -16,51 +20,105 @@ void printList(struct Node* node) {
     int i = 0;
 
     while (node != NULL) {
-        printf(" data:%d ", node->data);
+        printItem(node);
         node = node->next;
         i++;
-    }
-    
+    }    
 }
 
 void createList(struct Node* node, int size) {
     struct Node* current = node;
 
-    for (int i = 2; i < size + 1; i++)
+    for (int i = 1; i < size + 1; i++)
     {
         struct Node* node = malloc(sizeof(struct Node));
 
         node->data = i;
-        node->next = NULL;       
+        node->next = NULL;    
+
         current->next = node; 
         current = node;
     }
     
 }
 
+struct Node* last(struct Node* head)
+{
+    struct Node* lastItem = head;
+
+    while (lastItem->next != NULL)
+    {
+       lastItem = lastItem->next;
+    }
+    
+    return lastItem;
+}
+
+void add(struct Node* head, int data)
+{
+    struct Node* lastNode = last(head);
+
+    struct Node* newLastNode = malloc(sizeof(struct Node));
+
+    newLastNode->data = data;
+    newLastNode->next = NULL;    
+
+    lastNode->next = newLastNode;
+}
+
+struct Node* get(struct Node* head, int position)
+{
+    int i = 0;
+
+    struct Node* item = head;
+
+    while (i < position)
+    {
+        item = item->next;
+        i++;
+    }
+
+    return item;
+}
+
+void removeAt(struct Node* head, int position)
+{
+    int i = 0;
+    struct Node* previousItem = head;
+    struct Node* itemToRemove = head;
+
+    while (i < position)
+    {
+        previousItem = itemToRemove;
+        itemToRemove = itemToRemove->next;
+        i++;
+    }  
+
+    previousItem->next = itemToRemove->next;
+
+    free(itemToRemove);
+}
+
 void main()
 {
     struct Node* head = NULL;
-    // struct Node* second = NULL;
-    // struct Node* third = NULL;
-    
-
 
     head = malloc(sizeof(struct Node));
-    // second = malloc(sizeof(struct Node));
-    // third = malloc(sizeof(struct Node));
+    head->data = 0;
 
-    head->data = 1;
-    // head->next = *current;
-
-    // second->data = 2;
-    // second->next = third;
-
-    // third->data = 3;
-
-    
-    createList(head, 10);
-    
+    add(head, 1);
+    add(head, 2);
+    add(head, 3);
 
     printList(head);
+
+    removeAt(head, 2);
+
+    printList(head);
+
+    struct Node* itemAt = get(head, 1);
+    struct Node* lastItem = last(head);    
+
+    printItem(itemAt);
+    printItem(lastItem);
 }
