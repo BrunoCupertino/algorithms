980ddb14d4de365cea15cdc124c0b5bb63f7a302
= <=>
2020-05-24 16:02:02 -0300

    implemantation of containsKey and remove method

--- hashtable/hashtable.c
+++ hashtable/hashtable.c
@@ -46,16 +46,14 @@ int get(struct Hashtable* hashTable, char* key)
 
     int position = hashCode % M;
 
-    struct KeyValuePair **item = &*&*(hashTable->data + position);
+    struct KeyValuePair **item = (hashTable->data) + position;
 
     while (*item)
     {
-        // printf("key: %s\n", (*(item))->key);
-
-        if (strcmp((*(item))->key, key) == 0)
-            return (*(item))->value;
+        if (strcmp((*item)->key, key) == 0)
+            return (*item)->value;
         
-        *item = (*(item))->next;
+        item = &(*item)->next;
     }
 
     return -1;
@@ -67,36 +65,66 @@ void add(struct Hashtable* hashTable, char* key, int value)
 
     int position = hashCode % M;
 
-    struct KeyValuePair **item = &*&*(hashTable->data + position);
-
-    // printf("position: %d\n", position);
-    // printf("item: %p\n", item);
-    // printf("slot null?: %d\n", *item == NULL);
+    struct KeyValuePair **item = (hashTable->data) + position;
 
     struct KeyValuePair* newKeyValuePair = malloc(sizeof(struct KeyValuePair));
 
-    //TODO: handle collisions
     newKeyValuePair->key = key;
     newKeyValuePair->value = value;
     newKeyValuePair->next = *item;
     
-    if (!*item)
-        *item = newKeyValuePair;
-    else
-        (*(item))->next = newKeyValuePair;
+    *item = newKeyValuePair;
 
     hashTable->size++;
 }
 
 bool containsKey(struct Hashtable* hashTable, char* key)
 {
-    // int hashCode = hash(key);
+    int hashCode = hash(key);
 
-    // int position = hashCode % M;
+    int position = hashCode % M;
 
-    // int value = *(hashTable->data + position);
+    struct KeyValuePair **item = (hashTable->data) + position;
 
-    return true;
+    while (*item)
+    {
+        if (strcmp((*item)->key, key) == 0)
+            return true;
+
+        item = &(*item)->next;
+    }    
+
+    return false;
+}
+
+void removeItem(struct Hashtable* hashTable, char* key)
+{
+    int hashCode = hash(key);
+
+    int position = hashCode % M;
+
+    struct KeyValuePair **item = (hashTable->data) + position;
+    struct KeyValuePair **previousItem = item;
+
+    while (*item)
+    {
+        if (strcmp((*item)->key, key) == 0)
+        {
+            (*previousItem)->next = (*item)->next;
+
+            if (previousItem == item)
+                *previousItem = NULL;
+
+            free(*item);
+
+            hashTable->size--;
+
+            return;
+        }        
+        
+        previousItem = item;
+        item = &(*item)->next;
+    }    
 }
 
 int size(struct Hashtable* hashTable)
@@ -113,19 +141,34 @@ void main()
 {
     struct Hashtable* hashTable = create();
 
-    add(hashTable, "corinthians", 2);
-    add(hashTable, "barcelona", 3);
-    add(hashTable, "real madrid", 4);
-    add(hashTable, "liverpool", 1);
-    add(hashTable, "milan", 1);
-    add(hashTable, "bayern munich", 1);
-    add(hashTable, "palmeiras", 0);
+    char* corinthians = "corinthians";
+    char* barcelona = "barcelona";
+    char* realMadrid = "real madrid";
+    char* liverpool = "liverpool";
+    char* milan = "milan";
+    char* lazio = "lazio";
+    char* bayernMunich = "bayern munich";
+    char* palmeiras = "palmeiras";
 
-    printf("corinthians: %d\n", get(hashTable, "corinthians"));
-    printf("barcelona: %d\n", get(hashTable, "barcelona"));
-    printf("real madrid: %d\n", get(hashTable, "real madrid"));
-    printf("liverpool: %d\n", get(hashTable, "liverpool"));
-    printf("milan: %d\n", get(hashTable, "milan"));
-    printf("bayern munich: %d\n", get(hashTable, "bayern munich"));
-    printf("palmeiras: %d\n", get(hashTable, "palmeiras"));
+    add(hashTable, corinthians, 2);
+    add(hashTable, barcelona, 3);
+    add(hashTable, realMadrid, 4);
+    add(hashTable, liverpool, 1);
+    add(hashTable, bayernMunich, 1);
+    add(hashTable, palmeiras, 0);
+
+    printf("key: %s - value: %d\n", corinthians, get(hashTable, corinthians));
+    printf("key: %s - value: %d\n", barcelona, get(hashTable, barcelona));
+    printf("key: %s - value: %d\n", realMadrid, get(hashTable, realMadrid));
+    printf("key: %s - value: %d\n", liverpool, get(hashTable, liverpool));
+    printf("key: %s - value: %d\n", milan, get(hashTable, milan));
+    printf("key: %s - value: %d\n", bayernMunich, get(hashTable, bayernMunich));
+    printf("key: %s - value: %d\n", palmeiras, get(hashTable, palmeiras));
+
+    printf("contains key %s? %d\n", corinthians, containsKey(hashTable, corinthians));
+    printf("contains key %s? %d\n", barcelona, containsKey(hashTable, barcelona));
+    printf("contains key %s? %d\n", lazio, containsKey(hashTable, lazio));
+
+    removeItem(hashTable, bayernMunich);
+    printf("contains key %s? %d\n", bayernMunich, containsKey(hashTable, bayernMunich));
 }
